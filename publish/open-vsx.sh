d62cd2a47d0fecd2b5591a2349168c4fcb1b3b11
chenkai2 <chenkai2@staff.weibo.com>
2025-05-27 19:35:22 +0800

    chore: 更新open-vsx发布脚本
    * 添加工作目录变量设置
    * 修改为从当前目录读取package.json
    * 增加包名输出语句
    * 保持原有发布功能不变

--- publish/open-vsx.sh
+++ publish/open-vsx.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-name=$(grep '"name":' ../package.json | head -1 | awk -F: '{print $2}' | sed 's/[", ]//g')
-version=$(grep '"version":' ../package.json | head -1 | awk -F: '{print $2}' | sed 's/[", ]//g')
+workspaceFolder=$(cd $(dirname $0);cd ..;pwd)
+cd $workspaceFolder
+name=$(grep '"name":' package.json | head -1 | awk -F: '{print $2}' | sed 's/[", ]//g')
+version=$(grep '"version":' package.json | head -1 | awk -F: '{print $2}' | sed 's/[", ]//g')
 packageName="${name}-${version}.vsix"
+echo $packageName
 ovsx publish $packageName
