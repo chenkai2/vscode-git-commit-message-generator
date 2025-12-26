4bd3ed4cadc9bd0664d019cc359d9aa3d5e7ccdb
chenkai2 <chenkai2@staff.weibo.com>
2025-12-26 17:29:23 +0800

    chore: 更新发布脚本，添加版本管理说明
    内容如下：
    
    - 在 `publish/offical.sh` 中添加版本管理说明。
    - 说明如何递增主版本号、次版本号和修订号。
    - 提供了 `vsce publish` 命令的用法示例。

--- publish/offical.sh
+++ publish/offical.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 vsce package
 vsce publish
+
+# 进行不兼容的API更改时递增主版本号（第1位）
+# vsce publish major
+# 进行向后兼容的功能性更改时递增次版本号（第2位）
+# vsce publish minor
+# 进行向后兼容的问题修正时递增修订号（第3位）
+# vsce publish patch
